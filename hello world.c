d839f3553b3473df5ed26421ef2c7e050673cfcf
Ashwini Haral <67857972+ashharal@users.noreply.github.com>
2021-08-15 12:22:23 +0530

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,11 @@
+/******************************************************************************
+Name :- Haral Ashwini
+Assignment No : 1
+Write a program to print Hello World on output screen.
+*******************************************************************************/
+#include <stdio.h>
+int main(){
+    printf("hello world");
+    return 0;
+}
+//output: hello world
